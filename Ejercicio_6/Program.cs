fb77c069373d9159819ad959599e957adcf31657
alumno <alumno>
2018-08-23 20:05:04 -0300

    Comiteando

--- Ejercicio_6/Program.cs
+++ Ejercicio_6/Program.cs
@@ -10,6 +10,27 @@ namespace Ejercicio_6
     {
         static void Main(string[] args)
         {
+            int año;
+            string añoStr;
+            Console.WriteLine("Ingrese año");
+            añoStr = Console.ReadLine();
+            if(int.TryParse(añoStr,out año))
+                if ((año%4)==0)
+                {
+                    if(((año % 100) == 0) && ((año % 400) != 0))
+                    {
+                        Console.WriteLine("NO ES BISIESTO");
+                    }
+                    else
+                    {
+                         Console.WriteLine("Es bisiesto {0}", año);
+                    }
+                    //if (((año % 100) == 0) && ((año % 400) == 0))
+                      //  Console.WriteLine("Es bisiesto {0}", año);
+                }
+                else
+                    Console.WriteLine("NO ES BISIESTO");
+            Console.ReadKey();
         }
     }
 }
