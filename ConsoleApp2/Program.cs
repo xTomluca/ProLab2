7e80731fd09c393f155ec3fbde475b5960d8a2c3
alumno <alumno>
2018-08-23 22:07:52 -0300

    Commiteeeh

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int num=0;
+            int error = 1;
+            int promedio=0;
+            int max= int.MinValue;
+            int min=int.MaxValue;
+            string numStr;
+
+            for(i=0;i<10;i++)
+            {
+                Console.WriteLine("Ingrese numero: ");
+                numStr= Console.ReadLine();
+                if (int.TryParse(numStr, out num))
+                {
+                    if (Validacion.Validar(num, -100, 100))
+                    {
+                        if (num > max)
+                            max = num;
+                        if (num < min)
+                            min = num;
+                        promedio += num;
+                    }
+                    /*else
+                    {
+                        Console.WriteLine("ERROR!");
+                        error = 0;
+                        break;
+                    }*/
+
+                }
+                else
+                {
+                    Console.WriteLine("ERROR!");
+                    error = 0;
+                    break;
+                }
+            }
+            if(error==1)
+            {
+                promedio = promedio / 10;
+                Console.WriteLine("\n\nMaximo numero: {0} \nMinimo numero: {1} \nPromedio total: {2}", max,min,promedio);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia = 0;
+            int mes = 0;
+            int año=0;
+            int añoItr=0;
+            string diaStr;
+            string mesStr;
+            string añoStr;
+            DateTime ahora = DateTime.Now;
+            Console.WriteLine("Ingrese dia");
+            diaStr = Console.ReadLine();
+            Console.WriteLine("Ingrese mes");
+            mesStr = Console.ReadLine();
+            Console.WriteLine("Ingrese año");
+            añoStr = Console.ReadLine();
+            if((int.TryParse(diaStr,out dia))&& (int.TryParse(mesStr, out mes))&& (int.TryParse(añoStr, out año)))
+            {
+                añoItr = ahora.Year;
+                //for()
+                Console.WriteLine("AÑOS {0}", añoItr);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
