46f879f40310d5a9e7a777daf1144de25c15ebd5
alumno <alumno>
2018-08-30 20:29:37 -0300

    asdas

---
+++ Ejercicio 16 - 29 DIA/Ejercicio 16/Ejercicio 17/Boligrafo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_17
+{
+    public class Boligrafo
+    {
+        const short cantidadMaximaTinta = 100;
+        public ConsoleColor color = new ConsoleColor();
+        public short tinta;
+        ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+        short getTinta()
+        {
+            return this.tinta;
+        }
+        private void setTinta(short tinta)
+        {
+            if (tinta > 0 && (this.tinta + tinta) <= cantidadMaximaTinta)
+                this.tinta += tinta;
+            else if (tinta < 0 && (this.tinta - tinta) >= 0)
+                this.tinta -= tinta;
+        }
+        private void Recargar()
+        {
+            this.tinta = cantidadMaximaTinta;
+        }
+        public bool Pintar(int gasto, out string dibujo)
+        {
+            bool retorno = false;
+            dibujo = "";
+            //if (this.tinta > 0 && gasto > 0)
+            while(this.tinta>0 && gasto>0)
+            {
+                this.tinta--;
+                gasto--;
+                dibujo += "*";
+
+                /*for(int i=gasto; (i<this.tinta && this.tinta>0);i--)
+                {
+                    this.tinta--;
+                    dibujo += "*";
+                }*/
+                retorno = true;
+            }
+            return retorno;
+        }
+
+    }
+}
--- Ejercicio 16 - 29 DIA/Ejercicio 16/Ejercicio 17/Program.cs
+++ Ejercicio 16 - 29 DIA/Ejercicio 16/Ejercicio 17/Program.cs
@@ -10,6 +10,20 @@ namespace Ejercicio_17
     {
         static void Main(string[] args)
         {
+            string dibujo="";
+            //En el Main, crear un bolígrafo de tinta azul(ConsoleColor.Blue) y una cantidad inicial de tinta
+            //de 100 y otro de tinta roja(ConsoleColor.Red) y 50 de tinta
+            Boligrafo azul = new Boligrafo();
+            Boligrafo rojo = new Boligrafo();
+            azul.color = ConsoleColor.Blue;
+            azul.tinta = 100;
+            rojo.color = ConsoleColor.Red;
+            rojo.tinta = 50;
+            if (rojo.Pintar(10, out dibujo) == true)
+            {
+                Console.WriteLine("{0}\n Tinta actual: {1}", dibujo,rojo.tinta);
+            }
+            Console.ReadKey();
         }
     }
 }
