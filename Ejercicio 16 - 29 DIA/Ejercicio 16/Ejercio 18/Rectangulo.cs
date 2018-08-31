b09f4788a1f52fe5b486f872bb61edce635b0989
alumno <alumno>
2018-08-30 22:04:33 -0300

    asd

---
+++ Ejercicio 16 - 29 DIA/Ejercicio 16/Ejercio 18/Rectangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    class Rectangulo
+    {
+        float area;
+        float perimetro;
+        Punto vertice1;
+        Punto vertice2;
+        Punto vertice3;
+        Punto vertice4;
+        public Rectangulo(Punto vertice1, Punto vertice3)
+        {
+            
+        }
+    }
+}
