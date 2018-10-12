cb5f5ac87ff05c5fe894485f995ce2b9e4b09bb8
alumno <alumno>
2018-10-11 21:43:22 -0300

    asdas

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TryCatch prueba = new TryCatch("1024","0");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ TryCatch/TryCatch/TryCatch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class TryCatch
+    {
+        int num;
+        public static int MetodoExcepcion(string numStr, string numStr2)
+        {
+            int retorno = 0;
+            try
+            {
+                int num = int.Parse(numStr);
+                int num2 = int.Parse(numStr2);
+                retorno = num / num2;
+            }
+            catch (DivideByZeroException excepcion)
+            {
+                throw excepcion;
+            }
+            return retorno;
+        }
+        public string Mostrar()
+        {
+            string retorno = string.Format("El numero es:  {0}",this.num);
+            return retorno;
+        }
+        public TryCatch(string num1,string num2)
+        {
+            try
+            {
+                MetodoExcepcion(num1, num2);
+            }
+            catch(DivideByZeroException excepcion)
+            {
+                throw excepcion;
+            }
+        }
+        public TryCatch()
+        {
+            try
+            { 
+            }
+
+            catch(Exception c)
+            {
+                throw (new Exception("ERROR"));
+            }
+        }
+
+    }
+}
