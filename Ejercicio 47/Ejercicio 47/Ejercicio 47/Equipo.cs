0d26d710e52f0327546bce57cff1b7988b7def1e
alumno <alumno>
2018-10-24 20:07:27 -0300

    asdasdasd

--- Ejercicio 47/Ejercicio 47/Ejercicio 47/Equipo.cs
+++ Ejercicio 47/Ejercicio 47/Ejercicio 47/Equipo.cs
@@ -8,5 +8,25 @@ namespace Ejercicio_47
 {
     public class Equipo
     {
+        string nombre;
+        DateTime fechaCreacion;
+        public static bool operator ==(Equipo e1, Equipo e2)
+        {
+            if (e1.nombre == e2.nombre && e1.fechaCreacion == e2.fechaCreacion)
+                return true;
+            return false;
+        }
+        public static bool operator !=(Equipo e1, Equipo e2)
+        {
+            return !(e1 == e2);
+        }
+        //3. El método Ficha retornará el nombre del equipo y su fecha de creación con el
+        // siguiente formato “[EQUIPO]  fundado el[FECHA]”.
+        public string Ficha()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat("Nombre: {0}\nFecha Creacion: {1}", this.nombre, this.fechaCreacion.ToString());
+            return stringBuilder.ToString();
+        }
     }
 }
--- Ejercicio 47/Ejercicio 47/Ejercicio 47/Torneo.cs
+++ Ejercicio 47/Ejercicio 47/Ejercicio 47/Torneo.cs
@@ -16,7 +16,7 @@ namespace Ejercicio_47
     {
         List<T> equipos;
         string nombre;
-        public static bool operator ==(Equipo equipo, Torneo<T> torneo)
+        public static bool operator ==(T equipo, Torneo<T> torneo)
         {
             foreach (Equipo aux in torneo.equipos)
             {
@@ -25,11 +25,11 @@ namespace Ejercicio_47
             }
             return false;
         }
-        public static bool operator !=(Equipo equipo, Torneo<T> torneo)
+        public static bool operator !=(T equipo, Torneo<T> torneo)
         {
             return !(equipo==torneo);
         }
-        public static Torneo<T> operator +(Equipo equipo, Torneo<T> torneo)
+        public static Torneo<T> operator +(T equipo, Torneo<T> torneo)
         {
             if(equipo == torneo)
             {
@@ -38,10 +38,34 @@ namespace Ejercicio_47
             torneo.equipos.Add((T)equipo);
             return torneo;
         }
+            /*El método Mostrar retornará los datos del torneo y de los equipos
+            participantes.
+            6. El método privado CalcularPartido<T, T>: string recibirá dos elementos del
+            tipo T, que deberán ser del tipo Equipo o sus herencias, y calculará utilizando
+            Random un resultado del partido. Retornará el resultado con el siguiente
+            formato: “[EQUIPO1] [RESULTADO1] – [RESULTADO2] [EQUIPO2]”, siendo
+            EQUIPOX el nombre del equipo y RESULTADOX la cantidad de goles/puntos.*/
         public string Mostrar()
         {
             StringBuilder str = new StringBuilder();
-            str.
+            str.AppendFormat("Nombre Torneo: {0}");
+            foreach(Equipo aux in this.equipos)
+            {
+                str.Append(aux.Ficha());
+            }
+            return str.ToString();
         }
+        private string CalcularPartido(T a, T b) 
+        {
+
+            return "";
+        }
+            /* El método privado CalcularPartido<T, T>: string recibirá dos elementos del
+            tipo T, que deberán ser del tipo Equipo o sus herencias, y calculará utilizando
+            Random un resultado del partido. Retornará el resultado con el siguiente
+            formato: “[EQUIPO1] [RESULTADO1] – [RESULTADO2] [EQUIPO2]”, siendo
+            EQUIPOX el nombre del equipo y RESULTADOX la cantidad de goles/puntos.
+            7. La propiedad pública JugarPartido tomará dos equipos de la lista al azar y
+            calculará el resultado del partido a través del método CalcularPartido.*/
     }
 }
