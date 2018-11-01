0b532ff6b095fda7c0d986ef50f89f05ce4ce2e9
alumno <alumno>
2018-10-31 22:13:36 -0300

    a

---
+++ Ejercicio 57/Persona.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Ejercicio_57
+{
+    public class Persona
+    {
+        string nombre;
+        string apellido;
+        public string Nombre { get { return this.nombre; } set { this.nombre = value; } }
+        public string Apellido { get { return this.apellido; } set { this.apellido = value; } }
+        public Persona() { }
+        public Persona(string nombre, string apellido) : this()
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+        public static void Guardar(Persona persona)
+        {
+            XmlTextWriter writer;
+            XmlSerializer serializer;
+            writer = new XmlTextWriter("ArchivoXml.xml",Encoding.UTF8);
+            serializer = new XmlSerializer(typeof(Persona));
+            serializer.Serialize(writer, persona);
+            writer.Close();
+        }
+        public static Persona Leer()
+        {
+            XmlTextReader reader;
+            XmlSerializer serializer = new XmlSerializer(typeof(Persona));
+            reader = new XmlTextReader("ArchivoXml.xml");
+            Persona aux;
+            aux = (Persona)serializer.Deserialize(reader);
+            return aux;
+        }
+        public override string ToString()
+        {
+            Persona a = Persona.Leer();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat("Nombre : {0} {1}",a.Apellido, a.nombre);
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Ejercicio 57/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona("Jose", "Tester");
+            Persona.Guardar(p);
+            Console.WriteLine(p.ToString());
+            Console.ReadKey();
+        }
+    }
+}
